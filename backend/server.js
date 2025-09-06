016735a243e4f91350c9a03f504e99fb81502af9
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-09-07 02:08:08 +0530

    refactor: update CORS configuration to use the correct production URL

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:5173',  // Your Vite dev server
-    'https://pulseplay-8e09.onrender.com', 
+    'https://music-pulseplay.onrender.com', 
   
   ],
   credentials: true, // Allow cookies
