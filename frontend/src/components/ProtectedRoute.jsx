8a5446c5bc32097b8eddaa9e01c452bb2557fb45
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-09-06 13:12:53 +0530

    imporved peotected route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,31 +3,36 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
 
-const ProtectedRoute = ({children}) => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const navigate = useNavigate()
+const ProtectedRoute = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        const verifyUser = async () => {
-            try {
-                await axios.get("https://pulseplay-8e09.onrender.com/api/verify", {
-                    withCredentials: true
-                })
-                setIsAuthenticated(true)
-            } catch (error) {
-                setIsAuthenticated(false)
-            } finally {
-                setIsLoading(false)
-            }
-        }
-        verifyUser();
-    }, [])
-    if (isLoading) return <div>Loading...</div>;
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        axios.get("https://pulseplay-8e09.onrender.com/api/verify", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+          }
+        })
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
 
-    if (!isAuthenticated) return <Navigate to="/login" />;
+        setIsAuthenticated(true)
+      } catch (error) {
+        setIsAuthenticated(false)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    verifyUser();
+  }, [])
+  if (isLoading) return <div>Loading...</div>;
 
-    return children;
+  if (!isAuthenticated) return <Navigate to="/login" />;
+
+  return children;
 }
 
 export default ProtectedRoute
